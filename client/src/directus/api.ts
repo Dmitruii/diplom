065aeca4a51ff340f6c16fa9d42b57819bfa28ff
Dima Naymenko <dimanaymenko2005@gmail.com>
2024-09-16 13:21:03 +0300

    fix: eslint error

--- client/src/directus/api.ts
+++ client/src/directus/api.ts
@@ -2,7 +2,7 @@ import { authentication, createDirectus, graphql, rest } from "@directus/sdk";
 
 class LocalStorage {
   get() {
-    return JSON.parse(localStorage.getItem("directus-data"));
+    return JSON.parse(localStorage.getItem("directus-data") as string);
   }
   set(data: any) {
     localStorage.setItem("directus-data", JSON.stringify(data));
